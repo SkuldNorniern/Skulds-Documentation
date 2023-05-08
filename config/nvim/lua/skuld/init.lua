9747da4e6f1787d59b30318cbddb569ead92b833
WieeRd <wowluke@naver.com>
2023-05-08 20:09:35 +0900

    [FIX] fix broken API calls

--- config/nvim/lua/skuld/init.lua
+++ config/nvim/lua/skuld/init.lua
@@ -1,5 +1,12 @@
-local M = {}
+local cfg_root = ...
 local vim = vim
+
+local M = {
+  options = require(cfg_root .. ".builtin.options"),
+  keymaps = require(cfg_root .. ".builtin.keymaps"),
+  autocmds = require(cfg_root .. ".builtin.autocmds"),
+  commands = require(cfg_root .. ".builtin.commands"),
+}
 
 M.load_options = function(options)
   local opt = vim.opt
@@ -19,10 +26,10 @@ end
 
 M.load_autocmds = function(autocmds)
   local autocmd = vim.api.nvim_create_autocmd
-  local augroup = vim.api.nvim_create_augroup("Skuld")
-
+  local augroup = vim.api.nvim_create_augroup("Skuld", { clear = true })
   for _, opts in ipairs(autocmds) do
     local event = opts[1]
+    opts[1] = nil
     opts.group = augroup
     autocmd(event, opts)
   end
@@ -32,6 +39,7 @@ M.load_commands = function(commands)
   local command = vim.api.nvim_create_user_command
   for name, opts in pairs(commands) do
     local cmd = opts[1]
+    opts[1] = nil
     command(name, cmd, opts)
   end
 end
@@ -39,15 +47,17 @@ end
 M.setup = function(cfg)
   vim.g.mapleader = cfg.mapleader
 
-  local options = require("skuld.builtin.options")
-  local keymaps = require("skuld.builtin.keymaps")
-  local autocmds = require("skuld.builtin.autocmds")
-  local commands = require("skuld.builtin.commands")
+  M.load_options(M.options)
+  M.load_keymaps(M.keymaps)
+  M.load_autocmds(M.autocmds)
+  M.load_commands(M.commands)
 
-  M.load_options(options)
-  M.load_keymaps(keymaps)
-  M.load_autocmds(autocmds)
-  M.load_commands(commands)
+  if cfg.plugins then
+    require(cfg_root .. ".plugin")
+    vim.cmd.colorscheme(cfg.colorscheme.plugin)
+  else
+    vim.cmd.colorscheme(cfg.colorscheme.builtin)
+  end
 end
 
 return M
