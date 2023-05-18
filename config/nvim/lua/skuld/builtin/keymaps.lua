84ad8ab3464f468dc24e21d42cc8c09307a59f64
WieeRd <wowluke@naver.com>
2023-05-19 03:37:16 +0900

    [FEATURE] add `<Cmd>` wrapper util to keymaps

--- config/nvim/lua/skuld/builtin/keymaps.lua
+++ config/nvim/lua/skuld/builtin/keymaps.lua
@@ -1,6 +1,16 @@
+local function cmd(s)
+  return ("<Cmd>%s<CR>"):format(s)
+end
+
 return {
   i = {
     -- ESC alternative
     ["kj"] = "<Esc>",
+  },
+  n = {
+    ["<Leader>w"] = cmd("write"),
+    ["<Leader>W"] = cmd("wall"),
+    ["<Leader>q"] = cmd("quit"),
+    ["<Leader>Q"] = cmd("quit!"),
   },
 }
